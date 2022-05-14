442dfd2fbcbf14e5b677873f8b81bdee86e09aa2
jcouch <jasoncouchjr410@gmail.com>
2022-05-13 22:14:46 -0400

    added models and user controller

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,65 @@
 const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (date) => {
+      return `${new Date(date).getMonth() + 1}/${new Date(
+        date
+      ).getDate()}/${new Date(date).getFullYear()}`;
+    },
+  },
+});
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => {
+        return `${new Date(date).getMonth() + 1}/${new Date(
+          date
+        ).getDate()}/${new Date(date).getFullYear()}`;
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+thoughtsSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('thoughts', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,1 +1,44 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, Types, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: Thought,
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: User,
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
